3e1d30a639b9c60cdb2cca8cafbf6ba948d205a6
baba5465 <rishikesh.tiwari5465@gmail.com>
2020-04-25 19:06:18 +0530

    add note

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const fs = require ('fs');
-const os = require ('os');
+const _ = require ('lodash');
+const yargs = require ('yargs');
+
 const notes = require ('./notes.js');
 
-var user = os.userInfo ();
-fs.appendFileSync ('greetings.txt', '\nHello World!' + user.username);
+const argv = yargs.argv;
+var command = process.argv[2];
+
+if (command === 'add') {
+  notes.addNote (argv.title, argv.body);
+} else if (command === 'list') {
+} else if (command === 'remove') {
+} else if (command === 'read') {
+} else {
+  console.log ('Command not found');
+}
